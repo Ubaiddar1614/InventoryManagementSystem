e1c1b40b31a81efbe5191bb3c65382331b9bd8d2
Ubaiddar1614 <ubaiddar1614@gmail.com>
2026-05-09 17:27:06 +0500

    BOOM!!!!!!!!!!!Dashboard fully operational, backend architecture complete.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,44 @@
-using InventoryManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InventoryManagementSystem.Data;
+using InventoryManagementSystem.Models;
 using System.Diagnostics;
 
 namespace InventoryManagementSystem.Controllers
 {
+    [Microsoft.AspNetCore.Authorization.Authorize] 
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var totalProducts = await _context.Products.CountAsync();
+
+            var lowStockItems = await _context.Products
+                .Where(p => p.StockQuantity <= p.ReorderLevel)
+                .CountAsync();
+
+            var totalInventoryValue = await _context.Products
+                .SumAsync(p => p.Price * p.StockQuantity);
+
+            var totalSalesRevenue = await _context.Transactions
+                .Where(t => t.TransactionType == "Out")
+                .SumAsync(t => t.TotalAmount);
+
+            var dashboardData = new DashboardViewModel
+            {
+                TotalProducts = totalProducts,
+                LowStockItems = lowStockItems,
+                TotalInventoryValue = totalInventoryValue,
+                TotalSalesRevenue = totalSalesRevenue
+            };
+
+            return View(dashboardData);
         }
 
         public IActionResult Privacy()
---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagementSystem.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalProducts { get; set; }
+        public int LowStockItems { get; set; }
+        public decimal TotalInventoryValue { get; set; }
+        public decimal TotalSalesRevenue { get; set; }
+    }
+}
